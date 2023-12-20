6b4e7d6275cb2b32fc808069158abe4755495144
Dmitri Zagorulko <133781357+DmitriZag@users.noreply.github.com>
2023-12-20 22:39:45 +0300

    Update Main.java

--- src/main/java/ru/dmitrizag/Main.java
+++ src/main/java/ru/dmitrizag/Main.java
@@ -2,6 +2,6 @@ package ru.dmitrizag;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello qa.guru!");
+        System.out.printf("Hello qa.guru11111!");
         }
     }
