caf0164ae8b64d4910130db3da2d26bb091c883f
Dmitrii Zagorulko <dmitryzag88@gmail.com>
2023-12-20 21:39:52 +0300

    hello message was changed

--- src/main/java/ru/dmitrizag/Main.java
+++ src/main/java/ru/dmitrizag/Main.java
@@ -2,7 +2,6 @@ package ru.dmitrizag;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello qa.guru!");
         }
     }
-}
